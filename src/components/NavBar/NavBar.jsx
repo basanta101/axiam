7cb938d88aa101e2b3edcab37cd883a5b52fdc5b
noob_bokhonto <bsbickysharma3@gmail.com>
2024-05-10 16:04:16 +0530

    navbar done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import Button from "../Button/Button";
 import { BUTTON_VARIANT } from "../Button/Button.constants";
 
 const Navbar = () => {
-    return <nav className='flex flex-ac flex-jb p-lr-108 sticky-top'><h1 className="f-w-600 f-24">axiamatic</h1> <Button variant={BUTTON_VARIANT.OUTLINE}>Exit Setup</Button> </nav>
+    return <nav className='flex flex-ac flex-jb p-lr-24 sticky-top'><h1 className="f-w-600 f-24">axiamatic</h1> <Button variant={BUTTON_VARIANT.OUTLINE}>Exit Setup</Button> </nav>
 }
 
 export default Navbar;
